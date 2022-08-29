91360803d1432bcf27db0626cf9fd55c9e032374
samperozek <samperozek@gmail.com>
2022-08-29 14:06:22 -0400

    updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,55 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "🏕 Seeding dealers..."
+dealer1 = Dealer.create(name: 'Crazy Vans R Us')
+dealer2 = Dealer.create(name: 'Lizzie')
+dealer3 = Dealer.create(name: 'Tom')
+dealer4 = Dealer.create(name: 'Morgan')
+dealer5 = Dealer.create(name: 'Danny')
+dealer6 = Dealer.create(name: 'Peter')
+dealer7 = Dealer.create(name: 'Amanda')
+dealer8 = Dealer.create(name: 'Nick')
+
+puts "🏕 Seeding users..."
+user1 = User.create(username: 'Archery', password: "123")
+user2 = User.create(username: 'Swimming', password: "123")
+user3 = User.create(username: 'Photography', password: "123")
+user4 = User.create(username: 'Arts & Crafts', password: "123")
+user5 = User.create(username: 'Kayaking', password: "123")
+user6 = User.create(username: 'Fencing', password: "123")
+user7 = User.create(username: 'Canoeing', password: "123")
+user8 = User.create(username: 'Windsurfing', password: "123")
+
+puts "🏕 Seeding vans..."
+Van.create(dealer_id: dealer1.id, user_id: user2.id, year: 2018, make: "GMC", model: "Savana", price: 28999, imageURL: "https://lowgravityascents.com/wp-content/uploads/2014/09/Van.jpg")
+Van.create(dealer_id: dealer1.id, user_id: user1.id, year: 2018, make: "GMC", model: "Savana", price: 28999, imageURL: "https://lowgravityascents.com/wp-content/uploads/2014/09/Van.jpg")
+Van.create(dealer_id: dealer1.id, user_id: user4.id, year: 2018, make: "GMC", model: "Savana", price: 28999, imageURL: "https://lowgravityascents.com/wp-content/uploads/2014/09/Van.jpg")
+Van.create(dealer_id: dealer2.id, user_id: user2.id, year: 2018, make: "GMC", model: "Savana", price: 28999, imageURL: "https://lowgravityascents.com/wp-content/uploads/2014/09/Van.jpg")
+Van.create(dealer_id: dealer2.id, user_id: user1.id, year: 2018, make: "GMC", model: "Savana", price: 28999, imageURL: "https://lowgravityascents.com/wp-content/uploads/2014/09/Van.jpg")
+Van.create(dealer_id: dealer4.id, user_id: user8.id, year: 2018, make: "GMC", model: "Savana", price: 28999, imageURL: "https://lowgravityascents.com/wp-content/uploads/2014/09/Van.jpg")
+Van.create(dealer_id: dealer5.id, user_id: user7.id, year: 2018, make: "GMC", model: "Savana", price: 28999, imageURL: "https://lowgravityascents.com/wp-content/uploads/2014/09/Van.jpg")
+Van.create(dealer_id: dealer3.id, user_id: user4.id, year: 2018, make: "GMC", model: "Savana", price: 28999, imageURL: "https://lowgravityascents.com/wp-content/uploads/2014/09/Van.jpg")
+Van.create(dealer_id: dealer1.id, user_id: user2.id, year: 2018, make: "GMC", model: "Savana", price: 28999, imageURL: "https://lowgravityascents.com/wp-content/uploads/2014/09/Van.jpg")
+Van.create(dealer_id: dealer1.id, user_id: user1.id, year: 2018, make: "GMC", model: "Savana", price: 28999, imageURL: "https://lowgravityascents.com/wp-content/uploads/2014/09/Van.jpg")
+Van.create(dealer_id: dealer1.id, user_id: user4.id, year: 2018, make: "GMC", model: "Savana", price: 28999, imageURL: "https://lowgravityascents.com/wp-content/uploads/2014/09/Van.jpg")
+Van.create(dealer_id: dealer2.id, user_id: user2.id, year: 2018, make: "GMC", model: "Savana", price: 28999, imageURL: "https://lowgravityascents.com/wp-content/uploads/2014/09/Van.jpg")
+Van.create(dealer_id: dealer2.id, user_id: user1.id, year: 2018, make: "GMC", model: "Savana", price: 28999, imageURL: "https://lowgravityascents.com/wp-content/uploads/2014/09/Van.jpg")
+Van.create(dealer_id: dealer4.id, user_id: user8.id, year: 2018, make: "GMC", model: "Savana", price: 28999, imageURL: "https://lowgravityascents.com/wp-content/uploads/2014/09/Van.jpg")
+Van.create(dealer_id: dealer5.id, user_id: user7.id, year: 2018, make: "GMC", model: "Savana", price: 28999, imageURL: "https://lowgravityascents.com/wp-content/uploads/2014/09/Van.jpg")
+Van.create(dealer_id: dealer3.id, user_id: user4.id, year: 2018, make: "GMC", model: "Savana", price: 28999, imageURL: "https://lowgravityascents.com/wp-content/uploads/2014/09/Van.jpg")
+Van.create(dealer_id: dealer1.id, user_id: user2.id, year: 2018, make: "GMC", model: "Savana", price: 28999, imageURL: "https://lowgravityascents.com/wp-content/uploads/2014/09/Van.jpg")
+Van.create(dealer_id: dealer1.id, user_id: user1.id, year: 2018, make: "GMC", model: "Savana", price: 28999, imageURL: "https://lowgravityascents.com/wp-content/uploads/2014/09/Van.jpg")
+Van.create(dealer_id: dealer1.id, user_id: user4.id, year: 2018, make: "GMC", model: "Savana", price: 28999, imageURL: "https://lowgravityascents.com/wp-content/uploads/2014/09/Van.jpg")
+Van.create(dealer_id: dealer2.id, user_id: user2.id, year: 2018, make: "GMC", model: "Savana", price: 28999, imageURL: "https://lowgravityascents.com/wp-content/uploads/2014/09/Van.jpg")
+Van.create(dealer_id: dealer2.id, user_id: user1.id, year: 2018, make: "GMC", model: "Savana", price: 28999, imageURL: "https://lowgravityascents.com/wp-content/uploads/2014/09/Van.jpg")
+Van.create(dealer_id: dealer4.id, user_id: user8.id, year: 2018, make: "GMC", model: "Savana", price: 28999, imageURL: "https://lowgravityascents.com/wp-content/uploads/2014/09/Van.jpg")
+Van.create(dealer_id: dealer5.id, user_id: user7.id, year: 2018, make: "GMC", model: "Savana", price: 28999, imageURL: "https://lowgravityascents.com/wp-content/uploads/2014/09/Van.jpg")
+Van.create(dealer_id: dealer3.id, user_id: user4.id, year: 2018, make: "GMC", model: "Savana", price: 28999, imageURL: "https://lowgravityascents.com/wp-content/uploads/2014/09/Van.jpg")
+Van.create(dealer_id: dealer1.id, user_id: user2.id, year: 2018, make: "GMC", model: "Savana", price: 28999, imageURL: "https://lowgravityascents.com/wp-content/uploads/2014/09/Van.jpg")
+Van.create(dealer_id: dealer1.id, user_id: user1.id, year: 2018, make: "GMC", model: "Savana", price: 28999, imageURL: "https://lowgravityascents.com/wp-content/uploads/2014/09/Van.jpg")
+Van.create(dealer_id: dealer1.id, user_id: user4.id, year: 2018, make: "GMC", model: "Savana", price: 28999, imageURL: "https://lowgravityascents.com/wp-content/uploads/2014/09/Van.jpg")
+Van.create(dealer_id: dealer2.id, user_id: user2.id, year: 2018, make: "GMC", model: "Savana", price: 28999, imageURL: "https://lowgravityascents.com/wp-content/uploads/2014/09/Van.jpg")
+Van.create(dealer_id: dealer2.id, user_id: user1.id, year: 2018, make: "GMC", model: "Savana", price: 28999, imageURL: "https://lowgravityascents.com/wp-content/uploads/2014/09/Van.jpg")
+Van.create(dealer_id: dealer4.id, user_id: user8.id, year: 2018, make: "GMC", model: "Savana", price: 28999, imageURL: "https://lowgravityascents.com/wp-content/uploads/2014/09/Van.jpg")
+Van.create(dealer_id: dealer5.id, user_id: user7.id, year: 2018, make: "GMC", model: "Savana", price: 28999, imageURL: "https://lowgravityascents.com/wp-content/uploads/2014/09/Van.jpg")
+Van.create(dealer_id: dealer3.id, user_id: user4.id, year: 2018, make: "GMC", model: "Savana", price: 28999, imageURL: "https://lowgravityascents.com/wp-content/uploads/2014/09/Van.jpg")
+
+puts "✅ Done seeding!"
