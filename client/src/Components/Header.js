d754506a9ac3974e5b1460088757ccc089b0c044
samperozek <samperozek@gmail.com>
2022-09-14 13:20:57 -0500

    added rickroll to header image

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,11 +1,15 @@
 
 import React from "react";
+import { Link } from "react-router-dom";
 
 
 function Header () {
     return(
         <div>
-            <img src="https://www.seekpng.com/png/detail/89-890024_red-volkswagen-camper-van-png-stickpng-transport-vw.png" alt="Captain Planet" id="main-header" />
+            <img 
+            onClick={()=> window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank")}
+            src="https://www.seekpng.com/png/detail/89-890024_red-volkswagen-camper-van-png-stickpng-transport-vw.png" alt="Captain Planet" id="main-header" />
+           
             <h1 id = "headerText">Vanvana</h1>
             <h4 id = "headerText">When living in a van down by the river was a joke...</h4>
         </div>
